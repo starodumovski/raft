8f724a45ee5f3bf5d4eba17d4f4d8546fa78c6da
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-29 19:05:43 +0300

    feat: client re

--- client.py
+++ client.py
@@ -7,8 +7,8 @@ reg_suspend = r"suspend \d+"
 reg_connect = r"connect \d+.\d+.\d+.\d+ \d+"
 reg_leader = r"getleader"
 
-reg_get_value = r"getval \s+ \s+"
-reg_set_value = r"setval \s+"
+reg_get_value = r"getval \s+"
+reg_set_value = r"setval \s+ \s+"
 
 
 def parse_string(stub: pb2_grpc.NodeStub, message: str):
@@ -29,12 +29,12 @@ def parse_string(stub: pb2_grpc.NodeStub, message: str):
         elif re.fullmatch(reg_get_value, message):
             data = message.split(" ")
             key_ = data[1]
-            value_ = data[2]
             # TODO
             return None
         elif re.fullmatch(reg_set_value, message):
             data = message.split(" ")
             key_ = data[1]
+            value_ = data[2]
             # TODO
             return None
         return None
